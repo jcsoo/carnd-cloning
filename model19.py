f1e084c72289a00f8f0cbec9cec34c11258f7e58
Jonathan Soo <jcsoo@agora.com>
2017-11-24 21:12:33 +0000

    Model19 - remove y corrective factor

--- model19.py
+++ model19.py
@@ -44,7 +44,7 @@ def main(args):
                         validation_data=vg,
                         nb_val_samples=len(validation_samples),
                         nb_epoch=3)
-    model.save('model18.h5')
+    model.save('model19.h5')
 
 if __name__=='__main__':
     main(sys.argv[1:])
