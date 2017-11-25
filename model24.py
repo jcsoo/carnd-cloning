e18d8ea5f29b9331676f08cb6d3c6605192c7282
Jonathan Soo <jcsoo@agora.com>
2017-11-25 00:43:50 +0000

    Model 24 - Reduce exclusions, 4 epoch training

--- model24.py
+++ model24.py
@@ -53,7 +53,7 @@ def main(args):
                         validation_data=vg,
                         nb_val_samples=len(validation_samples),
                         nb_epoch=4)
-    model.save('model23a.h5')
+    model.save('model24.h5')
 
 if __name__=='__main__':
     main(sys.argv[1:])
