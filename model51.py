f8b22fe8bc19cd3502716461e34ec4bcf186508b
Jonathan Soo <jcsoo@agora.com>
2017-11-30 14:26:12 +0000

    Model 51a - no track 2 recovery

--- model51.py
+++ model51.py
@@ -55,7 +55,7 @@ def main(args):
                         validation_data=vg,
                         nb_val_samples=len(validation_samples),
                         nb_epoch=5)
-    model.save('model51.h5')
+    model.save('model51a.h5')
 
 if __name__=='__main__':
     main(sys.argv[1:])
